787e938784828d0ed3f0009b076f2cfc2f6566ca
Ivan Shepelev <yonishepelev@gmail.com>
2025-03-13 18:23:23 +0200

    initialize Electron app with preload, main, and renderer processes

---
+++ src/main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow, ipcMain } = require("electron/main");
+
+const path = require("node:path");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  win.loadFile("./src/index.html");
+};
+
+app.whenReady().then(() => {
+  ipcMain.handle("ping", () => {
+    console.log("ping server");
+
+    return "pong";
+  });
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
